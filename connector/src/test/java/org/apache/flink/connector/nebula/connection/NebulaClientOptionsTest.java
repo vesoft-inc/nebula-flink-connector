aac3f1fde383b8eba76a244d44920fc4379e6183
Nicole00 <anqi.wang@vesoft.com>
2021-10-25 14:47:15 +0800

    add test

--- connector/src/test/java/org/apache/flink/connector/nebula/connection/NebulaClientOptionsTest.java
+++ connector/src/test/java/org/apache/flink/connector/nebula/connection/NebulaClientOptionsTest.java
@@ -1,4 +1,109 @@
-import junit.framework.TestCase;
-public class NebulaClientOptionsTest extends TestCase {
-  
+package org.apache.flink.connector.nebula.connection;
+
+
+import org.apache.flink.connector.nebula.utils.SslSighType;
+import org.junit.Test;
+
+public class NebulaClientOptionsTest {
+    @Test
+    public void testConfigAddress() {
+        NebulaClientOptions nebulaClientOptions =
+                new NebulaClientOptions.NebulaClientOptionsBuilder()
+                        .setGraphAddress("127.0.0.1:9669")
+                        .setMetaAddress("127.0.0.1:9559")
+                        .setUsername("root")
+                        .setPassword("nebula")
+                        .setConnectRetry(1)
+                        .setTimeout(1000)
+                        .setEnableGraphSsl(true)
+                        .setEnableMetaSsl(true)
+                        .setSslSignType(SslSighType.CA)
+                        .setCaSignParam("caCrtFile", "crtFile", "keyFile")
+                        .setSelfSignParam("crtFile", "keyFile", "password")
+                        .build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMetaAddressWithEmptyAddress() {
+        new NebulaClientOptions.NebulaClientOptionsBuilder()
+                .setGraphAddress("127.0.0.1:9669")
+                .setMetaAddress(null)
+                .build();
+    }
+
+    @Test
+    public void testGraphAddressWithEmptyAddress() {
+        new NebulaClientOptions.NebulaClientOptionsBuilder()
+                .setGraphAddress(null)
+                .setMetaAddress("127.0.0.1:9559")
+                .build();
+    }
+
+
+    @Test
+    public void testIsEnableGraphSsl() {
+        NebulaClientOptions nebulaClientOptions = new NebulaClientOptions
+                .NebulaClientOptionsBuilder()
+                .setGraphAddress(null)
+                .setMetaAddress("127.0.0.1:9559")
+                .setEnableGraphSsl(false)
+                .setEnableMetaSsl(true)
+                .setSslSignType(SslSighType.CA)
+                .setCaSignParam("caCrtFile", "crtFile", "keyFile")
+                .build();
+        assert (nebulaClientOptions.isEnableGraphSsl());
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullSslSighType() {
+        NebulaClientOptions nebulaClientOptions = new NebulaClientOptions
+                .NebulaClientOptionsBuilder()
+                .setGraphAddress(null)
+                .setMetaAddress("127.0.0.1:9559")
+                .setEnableGraphSsl(false)
+                .setEnableMetaSsl(true)
+                .setSslSignType(null)
+                .setCaSignParam("caCrtFile", "crtFile", "keyFile")
+                .build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNoSslSighType() {
+        NebulaClientOptions nebulaClientOptions = new NebulaClientOptions
+                .NebulaClientOptionsBuilder()
+                .setGraphAddress(null)
+                .setMetaAddress("127.0.0.1:9559")
+                .setEnableGraphSsl(false)
+                .setEnableMetaSsl(true)
+                .setCaSignParam("caCrtFile", "crtFile", "keyFile")
+                .build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetCaSignParam() {
+        NebulaClientOptions nebulaClientOptions = new NebulaClientOptions
+                .NebulaClientOptionsBuilder()
+                .setGraphAddress(null)
+                .setMetaAddress("127.0.0.1:9559")
+                .setEnableGraphSsl(false)
+                .setEnableMetaSsl(true)
+                .setSslSignType(SslSighType.CA)
+                .setSelfSignParam("crtFile", "keyFile", "password")
+                .build();
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetSelfSignParam() {
+        NebulaClientOptions nebulaClientOptions = new NebulaClientOptions
+                .NebulaClientOptionsBuilder()
+                .setGraphAddress(null)
+                .setMetaAddress("127.0.0.1:9559")
+                .setEnableGraphSsl(false)
+                .setEnableMetaSsl(true)
+                .setSslSignType(SslSighType.SELF)
+                .setCaSignParam("caCrtFile", "crtFile", "keyFile")
+                .build();
+    }
 }
--- connector/src/test/java/org/apache/flink/connector/nebula/connection/NebulaGraphConnectionProviderTest.java
+++ connector/src/test/java/org/apache/flink/connector/nebula/connection/NebulaGraphConnectionProviderTest.java
@@ -1,4 +1,81 @@
-import static org.junit.Assert.*;
+package org.apache.flink.connector.nebula.connection;
+
+import com.vesoft.nebula.client.graph.exception.AuthFailedException;
+import com.vesoft.nebula.client.graph.exception.ClientServerIncompatibleException;
+import com.vesoft.nebula.client.graph.exception.IOErrorException;
+import com.vesoft.nebula.client.graph.exception.NotValidConnectionException;
+import org.apache.flink.connector.nebula.utils.SslSighType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class NebulaGraphConnectionProviderTest {
-  
+    private static final Logger LOG =
+            LoggerFactory.getLogger(NebulaGraphConnectionProviderTest.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getSession() {
+        NebulaClientOptions nebulaClientOptions =
+                new NebulaClientOptions.NebulaClientOptionsBuilder()
+                        .setGraphAddress("127.0.0.1:9669")
+                        .setMetaAddress("127.0.0.1:9559")
+                        .setUsername("root")
+                        .setPassword("nebula")
+                        .setConnectRetry(1)
+                        .setTimeout(1000)
+                        .build();
+        NebulaGraphConnectionProvider graphConnectionProvider =
+                new NebulaGraphConnectionProvider(nebulaClientOptions);
+        try {
+            graphConnectionProvider.getSession();
+        } catch (Exception e) {
+            LOG.info("get session failed, ", e);
+            assert (false);
+        }
+    }
+
+    /**
+     * nebula server does not enable ssl, the connect cannot be established correctly.
+     */
+    @Test(expected = NotValidConnectionException.class)
+    public void getSessionWithSsl() throws NotValidConnectionException {
+        NebulaClientOptions nebulaClientOptions =
+                new NebulaClientOptions.NebulaClientOptionsBuilder()
+                        .setGraphAddress("127.0.0.1:9669")
+                        .setMetaAddress("127.0.0.1:9559")
+                        .setUsername("root")
+                        .setPassword("nebula")
+                        .setConnectRetry(1)
+                        .setTimeout(1000)
+                        .setEnableGraphSsl(true)
+                        .setEnableMetaSsl(true)
+                        .setSslSignType(SslSighType.CA)
+                        .setCaSignParam("src/test/resources/ssl/casigned.pem",
+                                "src/test/resources/ssl/casigned.crt",
+                                "src/test/resources/ssl/casigned.key")
+                        .build();
+
+
+        NebulaGraphConnectionProvider graphConnectionProvider =
+                new NebulaGraphConnectionProvider(nebulaClientOptions);
+
+        try {
+            graphConnectionProvider.getSession();
+        } catch (IOErrorException | AuthFailedException | ClientServerIncompatibleException e) {
+            LOG.error("get session faied, ", e);
+            assert (false);
+        }
+    }
+
+    // todo test ssl for server
 }

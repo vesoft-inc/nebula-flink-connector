02f7c1c748852926f61d1834423b9351582eef83
Nicole00 <anqi.wang@vesoft.com>
2021-10-25 14:49:07 +0800

    add copyright

--- connector/src/test/java/org/apache/flink/connector/nebula/connection/NebulaClientOptionsTest.java
+++ connector/src/test/java/org/apache/flink/connector/nebula/connection/NebulaClientOptionsTest.java
@@ -1,5 +1,10 @@
-package org.apache.flink.connector.nebula.connection;
+/* Copyright (c) 2021 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License,
+ * attached with Common Clause Condition 1.0, found in the LICENSES directory.
+ */
 
+package org.apache.flink.connector.nebula.connection;
 
 import org.apache.flink.connector.nebula.utils.SslSighType;
 import org.junit.Test;

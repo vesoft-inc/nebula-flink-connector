2749f7a8707cd533d70858b7f91eea845e63d262
spike.liu <spike.liu@outlook.com>
2022-07-05 15:28:35 +0800

    + licenses

--- connector/src/test/java/org/apache/flink/connector/nebula/sink/AbstractNebulaOutPutFormatITTest.java
+++ connector/src/test/java/org/apache/flink/connector/nebula/sink/AbstractNebulaOutPutFormatITTest.java
@@ -1,3 +1,8 @@
+/* Copyright (c) 2020 vesoft inc. All rights reserved.
+ *
+ * This source code is licensed under Apache 2.0 License.
+ */
+
 package org.apache.flink.connector.nebula.sink;
 
 import java.util.concurrent.ExecutionException;

ec930392b1fe51195af17f26dc2807d1d7caa92b
Anqi <anqi.wang@vesoft.com>
2024-04-07 18:00:13 +0800

    support auth for flink source

--- connector/src/main/java/org.apache.flink/connector/nebula/connection/NebulaStorageConnectionProvider.java
+++ connector/src/main/java/org.apache.flink/connector/nebula/connection/NebulaStorageConnectionProvider.java
@@ -56,6 +56,8 @@ public class NebulaStorageConnectionProvider implements Serializable {
             storageClient = new StorageClient(addresses, timeout);
         }
 
+        storageClient.setUser(nebulaClientOptions.getUsername());
+        storageClient.setPassword(nebulaClientOptions.getPassword());
         if (!storageClient.connect()) {
             throw new Exception("failed to connect storaged.");
         }

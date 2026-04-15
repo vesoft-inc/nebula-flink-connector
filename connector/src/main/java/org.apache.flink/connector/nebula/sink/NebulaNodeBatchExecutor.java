fffe308a849657dea070d78318321cab8c6fa295
Anqi <16240361+Nicole00@users.noreply.github.com>
2026-04-15 15:12:34 +0800

    update copyright date

--- connector/src/main/java/org.apache.flink/connector/nebula/sink/NebulaNodeBatchExecutor.java
+++ connector/src/main/java/org.apache.flink/connector/nebula/sink/NebulaNodeBatchExecutor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 vesoft inc. All rights reserved.
+ * Copyright (c) 2026 vesoft inc. All rights reserved.
  *
  * This source code is licensed under Apache 2.0 License.
  */
